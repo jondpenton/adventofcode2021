b671cdfab9c385ca7b222e615967b6375032c3a7
Jon Penton <hey@jondpenton.com>
2021-12-01 13:14:42 -0500

    feat: :sparkles: Day 1 part 1 solution

--- days/1/solution.ts
+++ days/1/solution.ts
@@ -1,3 +1,26 @@
-async function main() {}
+import path from 'path'
+import fs from 'fs/promises'
 
-main();
+async function main() {
+  const inputStr = await fs.readFile(path.resolve(__dirname, `./input.txt`), {
+    encoding: `utf-8`,
+  })
+  const measurements = inputStr
+    .split(/[\n\r]/)
+    .filter((line) => line !== ``)
+    .map(Number)
+  let increasedMeasurementCount = 0
+
+  for (let i = 1; i < measurements.length; i++) {
+    const previousMeasurement = measurements[i - 1]
+    const measurement = measurements[i]
+
+    if (previousMeasurement < measurement) {
+      increasedMeasurementCount++
+    }
+  }
+
+  console.log(increasedMeasurementCount)
+}
+
+main()
